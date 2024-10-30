435e7ebc023cabde8059a2a7c6a366f432f72013
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-30 16:22:04 +0100

    add: first object

---
+++ myapp/data.js
@@ -0,0 +1,11 @@
+/* const mySeries = [
+    {
+        title: "True Detective",
+        content: "Time is a flat circle",
+        image:"/express-blog-intro/myapp/img/rust.jpg",
+        tags: ["Crime", "Realistic"]
+    }
+    
+]
+
+console.log(mySeries); */
