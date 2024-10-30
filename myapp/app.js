2c234529f2a356e97075718b886a3baf0d631dba
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-30 17:24:38 +0100

    add: posts route

--- myapp/app.js
+++ myapp/app.js
@@ -3,9 +3,13 @@ const app = express()
 const port = 3000
 const host = "http://127.0.0.1"
 
+const mySeries = require("./controller/data.js")
+
 app.get('/', (req, res) => {
   res.send(`<h1>Benvenuto nel mio Blog!<h1>`)
 })
+
+app.get("/posts", mySeries.index)
 
 app.listen(port, () => {
   console.log(`Example app listening on ${host}:${port}`)
--- myapp/controller/data.js
+++ myapp/controller/data.js
@@ -2,32 +2,43 @@ const mySeries = [
     {
         title: "True Detective",
         content: "Time is a flat circle",
-        image:"./myapp/img/rust.jpg",
+        image: "./myapp/img/rust.jpg",
         tags: ["Crime", "Realistic"]
     },
     {
         title: "Dexter",
         content: "Just like me... empty inside",
-        image:"./myapp/img/dexter.jpg",
+        image: "./myapp/img/dexter.jpg",
         tags: ["Crime", "Thriller"]
     },
     {
         title: "Breaking Bad",
         content: "I'm not in danger, I am the danger!",
-        image:"./myapp/img/Walter_White_S5B.png",
+        image: "./myapp/img/Walter_White_S5B.png",
         tags: ["Crime", "Drugs"]
-    },{
+    },
+    {
         title: "Utopia",
         content: "Where is Jessica Hide?",
-        image:"./myapp/img/utopia-blogs.jpg",
+        image: "./myapp/img/utopia-blogs.jpg",
         tags: ["Distopyc", "Violence"]
-    },{
+    },
+    {
         title: "Luther",
         content: "I'm coming for you.",
-        image:"./myapp/img/luther.jpg",
+        image: "./myapp/img/luther.jpg",
         tags: ["Crime", "Realistic"]
-    },
-    
+    }
+
 ]
 
-console.log(mySeries);
+function index(req, res) {
+    res.json({
+        data: mySeries,
+        count: mySeries.length
+    })
+}
+
+module.exports = {
+    index
+}
