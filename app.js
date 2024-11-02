c8a818c683dfc6a88ab42be51e1bf634d5909730
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-02 15:29:10 +0100

    add: style on page

--- app.js
+++ app.js
@@ -6,19 +6,27 @@ const host = "http://127.0.0.1"
 
 const mySeries = require("./controller/data.js")
 
+const {title, image} = mySeries;
+
 app.get('/', (req, res) => {
   const markup = `
+  <div style= "text-align: center; margin: 1.5rem">
   <h1>Benvenuto nel mio Blog!<h1>
-  <img src= "./img/rust.jpg" alt = "True Detective" style = "width:400px; aspect-ratio:1">
-  <img src= "./img/dexter.jpg" alt = "Dexter" style = "width:400px; aspect-ratio:1">
-  <img src= "./img/Walter_White_S5B.png" alt = "Breaking Bad" style = "width:400px; aspect-ratio:1">
-  <img src= "/img/utopia-blogs.jpg" alt = "Utopia" style = "width:400px; aspect-ratio:1">
-  <img src= "./img/luther.jpg" alt = "Luther" style = "width:400px; aspect-ratio:1">
-  `
-
-
+  </div>
+  <div style= "gap: 1rem">
+  <img src= "./img/rust.jpg" alt = "True Detective" style = "width:400px; aspect-ratio:1; margin: 1rem">
+  <img src= "./img/dexter.jpg" alt = "Dexter" style = "width:400px; aspect-ratio:1; margin: 1rem">
+  <img src= "./img/Walter_White_S5B.png" alt = "Breaking Bad" style = "width:400px; aspect-ratio:1; margin: 1rem">
+  <img src= "/img/utopia-blogs.jpg" alt = "Utopia" style = "width:400px; aspect-ratio:1; margin: 1rem">
+  <img src= "./img/luther.jpg" alt = "Luther" style = "width:400px; aspect-ratio:1; margin: 1rem">
+  </div>
+  ` 
   res.send(markup)
-})
+  });
+
+
+  
+
 
 app.get("/posts", mySeries.index)
 
